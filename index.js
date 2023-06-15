82095f8d61393a1714aa5a6ed66d3fa2d19c971a
USER <+>
2023-06-16 00:15:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  console.log(mondayWork());
+  console.log(mondayWork("work online")); 
+
+function wrapAdjective(majestic = '*') {
+    return function (adjective = 'special'){
+      return `You are ${majestic}${adjective}${majestic}!`;  
+    }
+}
